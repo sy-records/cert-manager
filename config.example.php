c2e46b997d33689119394f2517b24bf57b5e1e44
Luffy <52o@qq52o.cn>
2024-12-30 14:58:53 +0800

    fix typo

--- config.example.php
+++ config.example.php
@@ -5,6 +5,6 @@ return [
         'resourceTypes' => ['cdn'],
         'fullchain' => '/www/server/panel/vhost/cert/example.com/fullchain.pem',
         'privkey' => '/www/server/panel/vhost/cert/example.com/privkey.pem',
-        'removeOldCert' => true,
+        'removeOld' => true,
     ]
 ];
