183ccfa46036ed65fb6f564a65e294ab859a6247
sy-records <52o@qq52o.cn>
2025-02-04 10:27:05 +0800

    Support merge and copy certificate

--- config.example.php
+++ config.example.php
@@ -5,6 +5,20 @@ return [
         'resourceTypes' => ['cdn'],
         'fullchain' => '/www/server/panel/vhost/cert/example.com/fullchain.pem',
         'privkey' => '/www/server/panel/vhost/cert/example.com/privkey.pem',
-        'removeOld' => true,
+        'removeOld' => true, // 删除旧证书
+        // 其他需要合并的证书路径
+        'otherFullchain' => [
+            '/www/server/panel/vhost/cert/example1.com/fullchain.pem',
+        ],
+        'otherPrivkey' => [
+            '/www/server/panel/vhost/cert/example1.com/otherprivkey.pem',
+        ],
+        // 复制到其他目录
+        'copyFullchainTo' => [
+            '/www/server/panel/vhost/cert/example2.com/fullchain.pem',
+        ],
+        'copyPrivkeyTo' => [
+            '/www/server/panel/vhost/cert/example2.com/privkey.pem',
+        ],
     ]
 ];
